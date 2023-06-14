b3ab42baf03074a3c81a08cf9a2a4f64406cb570
rikschoonbeek <rik.schoonbeek@gmail.com>
2023-06-15 00:04:58 +0200

    removed backend_old

--- backend_old/data_source/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DataSourceConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "data_source"
--- backend_old/user/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-
-
-class AppUser(AbstractUser):
-    # Add any additional fields or methods you need for your custom user model
-    pass
